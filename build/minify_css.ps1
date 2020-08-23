5c7b91076dbd92d4a652ad355894238204ff4578
Kevin Verreault <mail@kevinverreault.com>
2020-08-23 13:51:56 -0400

    minify scripts

---
+++ build/minify_css.ps1
@@ -0,0 +1,20 @@
+Param(    
+    [Parameter(Mandatory=$true)]    
+    [string]$folder  
+     )    
+    $libPath = $PSScriptRoot + "\lib\Yahoo.Yui.Compressor.dll"  
+    [Reflection.Assembly]::LoadFile($libPath)  
+      
+    $target = $PSScriptRoot + "\output\";
+    $files = get-childitem $folder -recurse -force -include *.css
+    Write-Host "Minifying CSS"
+    foreach($file in $files){   
+       $content = [IO.File]::ReadAllText($file.FullName)  
+       $cssCompressor = New-Object -TypeName Yahoo.Yui.Compressor.CssCompressor  
+       $compressedContent = $cssCompressor.Compress($content)  
+       $result = $target + $file.Name
+       #Set-ItemProperty $result -name IsReadOnly -value $false
+       Write-Host $result
+       [IO.File]::WriteAllText($result,$compressedContent)  
+    }  
+          
