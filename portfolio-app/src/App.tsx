94325b8e609078c17cc5402363c2a545f09815f2
Kevin Verreault <mail@kevinverreault.com>
2024-02-11 10:42:59 -0500

    Refactor index and app (#8)

--- portfolio-app/src/App.tsx
+++ portfolio-app/src/App.tsx
@@ -1,40 +1,17 @@
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import './index.css'
-import { Routes, Route } from 'react-router-dom'
-import Accueil from './Components/Gallery/Home/Accueil'
-import Faune from './Components/Gallery/Faune'
-import Paysages from './Components/Gallery/Paysages'
-import NavigationHeader from './Components/Header/NavigationHeader'
-import Contact from './Components/Forms/Contact'
-import { MetadataContext } from './Contexts/MetadataContext'
-import useMetadata from './Hooks/useMetadata'
-import { PostHogProvider } from 'posthog-js/react'
-import { usePageViewTracking } from './Hooks/useAnalytics'
+import Layout from './Layout'
 
 export default function App () {
-  const metadata = useMetadata()
-  usePageViewTracking()
-
   return (
-    <div className="App">
-      <MetadataContext.Provider value={metadata}>
-        <PostHogProvider
-          apiKey={process.env.REACT_APP_ANALYTICS_KEY}
-          options={{
-            api_host: process.env.REACT_APP_PUBLIC_ANALYTICS_HOST
-          }}>
-          <NavigationHeader />
-            <Routes>
-                <Route path="/" element={<Accueil />} />
-                <Route path="/faune" element={<Faune />} />
-                <Route path="/paysages" element={<Paysages />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-          </PostHogProvider>
-        </MetadataContext.Provider>
+    <BrowserRouter>
+      <div className="App">
+        <Layout />
         <footer>
             <span>© 2024 Kevin Verreault</span>
         </footer>
-    </div>
+      </div>
+    </BrowserRouter>
   )
 }
