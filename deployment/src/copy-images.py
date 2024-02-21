f4edfedfcc5ad7447342dea604406f41072a8219
Kevin Verreault <mail@kevinverreault.com>
2024-02-20 19:43:11 -0500

    Add copy-images script and metadata file

--- deployment/src/copy-images.py
+++ deployment/src/copy-images.py
@@ -2,55 +2,74 @@ import os
 import shutil
 import re
 
-# Constants for source and root destination paths
-ROOT_PATH = r"C:\Users\Kevin\Pictures\JPEG\Autre\siteweb"  # Update this path with the root source directory
-ROOT_DESTINATION_PATH = r"D:\code\portfolio-web-app\image-sources"  # Update this path for the root destination
+try:
+    ROOT_PATH = os.environ["PORTFOLIO_IMAGES_SOURCE_PATH"]
+    ROOT_DESTINATION_PATH = os.environ["PORTFOLIO_IMAGES_DESTINATION_PATH"]
+except KeyError as e:
+    print(f"Required environment variable {e.args[0]} is not set. Aborting...")
+    raise SystemExit(1) 
 
-# Keywords and their corresponding subdirectories at the destination
 DESTINATION_SUBDIRECTORIES = {
-    "Green": "fullsize",  # Subdirectory for "fullsize" images
-    "Orange": "thumbnail",  # Subdirectory for "thumbnail" images
+    "Green": "fullsize",
+    "Orange": "thumbnail",
 }
 
-# Ensure destination directories exist, create them if they don't
+
 def ensure_directory_exists(directory_path):
     if os.path.exists(directory_path):
         shutil.rmtree(directory_path)
     os.makedirs(directory_path)
 
-def sanitize_filename(filename):
-    # Replace double underscores with a single one
-    sanitized = re.sub(r"__+", "_", filename)
-    # Remove trailing underscore and number
-    sanitized = re.sub(r"_[0-9]+$", "", sanitized)
-    return sanitized
 
-def copy_files_based_on_keyword(keyword, source_sub_path, relative_path=""):
-    destination_subdir = DESTINATION_SUBDIRECTORIES[keyword]
-    destination_path = os.path.join(ROOT_DESTINATION_PATH, destination_subdir, relative_path)
-    print(destination_path)
+def sanitize_filename(filename):
+    sanitized = re.sub(r"__+", "_", filename)
+    base, ext = os.path.splitext(sanitized)
+    base = re.sub(r"_[0-9]_[0-9]+$", "", base)
+    base = re.sub(r"_[0-9]+$", "", base)
+    base = re.sub("_DSF", "_DSCF", base)
+    return base + ext
+
+
+def copy_files(source_sub_path, destination_path):
     ensure_directory_exists(destination_path)
     for filename in os.listdir(source_sub_path):
         full_file_path = os.path.join(source_sub_path, filename)
         if os.path.isfile(full_file_path):
             sanitized_filename = sanitize_filename(filename)
-            shutil.copy(full_file_path, os.path.join(destination_path, sanitized_filename))
+            shutil.copy(
+                full_file_path, os.path.join(destination_path, sanitized_filename)
+            )
 
-def process_directory(directory_path, relative_path=""):
+
+def process_directory(directory_path, keyword=None, relative_path=""):
     for entry in os.listdir(directory_path):
         full_path = os.path.join(directory_path, entry)
-        print(full_path)
         if os.path.isdir(full_path):
-            # Check if the directory name matches any of our keywords
-            if entry in DESTINATION_SUBDIRECTORIES:
-                copy_files_based_on_keyword(entry, full_path, relative_path)
-                print(full_path)
+            if entry in DESTINATION_SUBDIRECTORIES or keyword:
+                current_keyword = keyword if keyword else entry
+                new_relative_path = (
+                    os.path.join(relative_path, entry) if keyword else ""
+                )
+                destination_subdir = DESTINATION_SUBDIRECTORIES[current_keyword]
+                destination_path = os.path.join(
+                    ROOT_DESTINATION_PATH, destination_subdir, new_relative_path
+                )
+                if keyword:
+                    copy_files(full_path, destination_path)
+                else:
+                    process_directory(
+                        full_path,
+                        keyword=current_keyword,
+                        relative_path=new_relative_path,
+                    )
             else:
-                # Recurse into subdirectories that are not keyword directories
-                new_relative_path = os.path.join(relative_path, entry)
-                process_directory(full_path, new_relative_path)
+                process_directory(
+                    full_path, relative_path=os.path.join(relative_path, entry)
+                )
 
-# Start processing from the ROOT_PATH
+
 process_directory(ROOT_PATH)
 
-print("Files have been successfully copied based on their format.")
+print(
+    "Files have been successfully copied and filenames sanitized based on their format."
+)
